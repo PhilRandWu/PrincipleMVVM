d4abfb1090ebec833e489e5661dc473213bd75a0
PhilRandWu <3378814052@qq.com>
2022-02-15 14:23:08 +0800

    代理嵌套对象

--- Due/Instance/proxy.js
+++ Due/Instance/proxy.js
@@ -3,7 +3,7 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-02-15 11:07:20
- * @LastEditTime: 2022-02-15 11:29:26
+ * @LastEditTime: 2022-02-15 14:21:56
  * @LastEditors: PhilRandWu
  */
 /**
@@ -25,6 +25,13 @@ export function constructorProxy(vm, obj, namespace) {
     return proxyObj;
 }
 
+/**
+ * @description: 代理对象的构造函数
+ * @param {*} vm
+ * @param {*} obj
+ * @param {*} namespace
+ * @return {*}
+ */
 function constructorObjectProxy(vm, obj, namespace) {
     let proxyObj = {};
     for (let prop in obj) {
@@ -34,7 +41,7 @@ function constructorObjectProxy(vm, obj, namespace) {
                 return obj[prop];
             },
             set(value) {
-                console.log(value);
+                console.log(getNamespace(namespace, prop));
                 obj[prop] = value;
             }
         });
@@ -44,9 +51,25 @@ function constructorObjectProxy(vm, obj, namespace) {
                 return obj[prop];
             },
             set(value) {
+                console.log(getNamespace(namespace, prop));
                 obj[prop] = value;
             }
         })
+        if (obj[prop] instanceof Object) {
+            proxyObj[prop] = constructorObjectProxy(vm, obj[prop], getNamespace(namespace, prop));
+        }
     }
     return proxyObj;
+}
+
+
+function getNamespace(nowNameSpace, nowProp) {
+    if (nowNameSpace === null || nowNameSpace === '') {
+        return nowProp;
+    } else if (nowProp === null || nowProp === '') {
+        return nowNameSpace;
+    } else {
+        return nowNameSpace + '.' + nowProp;
+    }
+
 }
--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-02-14 20:17:53
- * @LastEditTime: 2022-02-15 11:28:12
+ * @LastEditTime: 2022-02-15 14:19:04
  * @LastEditors: PhilRandWu
  */
 import Due from '../Due/index.js';
@@ -12,7 +12,11 @@ const test = new Due({
     el: 'app',
     data: {
         content: 'key',
-        description: 'this is description'
+        description: 'this is description',
+        obj: {
+            a: 1,
+            b: 2022
+        }
     }
 });
 
