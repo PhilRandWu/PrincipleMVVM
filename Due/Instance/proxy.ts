b3311bd5bb14f0d9b558c3a408003e012b934920
PhilRandWu <3378814052@qq.com>
2022-05-06 17:34:40 +0800

    代理对象

--- Due/Instance/proxy.ts
+++ Due/Instance/proxy.ts
@@ -3,7 +3,7 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-05-06 16:49:07
- * @LastEditTime: 2022-05-06 17:12:13
+ * @LastEditTime: 2022-05-06 17:34:25
  * @LastEditors: PhilRandWu
  */
 import { options } from "./../type/options";
@@ -23,6 +23,7 @@ export function constructorProxy(vm, data: options["data"], namespace) {
     // 是一个对象，使用 Ts 类型检查机制，不必判断其他类型
     proxyObj = constructorObjectProxy(vm, data, namespace);
   }
+  return proxyObj;
 }
 
 /**
@@ -32,7 +33,32 @@ export function constructorProxy(vm, data: options["data"], namespace) {
  * @param {*} namespace
  * @return {*}
  */
-export function constructorObjectProxy(vm, data: options["data"], namespace) {}
+export function constructorObjectProxy(vm, data: options["data"], namespace) {
+    let proxyObj = null;
+    for (const key in data) {
+        Object.defineProperty(proxyObj,key,{
+            configurable:true,
+            get() {
+                return data[key];
+            },
+            set(value) {
+                console.log('set value',value);
+                data[key] = value;
+            }
+        })
+        // 进一步在 Due 上代理
+        Object.defineProperty(vm,key,{
+            configurable: true,
+            get() {
+                return data[key];
+            },
+            set(value) {
+                data[key] = value;
+            }
+        })
+    }
+    return proxyObj;
+}
 
 /**
  * @description: 处理代理数组
