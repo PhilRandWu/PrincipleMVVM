e7916c95f42d8a91c559cad1a8bcf7d42dc5c7a2
PhilRandWu <3378814052@qq.com>
2022-05-06 16:54:38 +0800

    代理data

---
+++ Due/Instance/init.ts
@@ -0,0 +1,30 @@
+import { options } from './../type/options';
+import { constructorProxy } from './constructorProxy';
+/*
+ * @Description: 初始化 Due 构造函数
+ * @Author: PhilRandWu
+ * @Github: https://github/PhilRandWu
+ * @Date: 2022-05-06 16:28:07
+ * @LastEditTime: 2022-05-06 16:54:05
+ * @LastEditors: PhilRandWu
+ */
+let uid = 0;
+
+export function init(Due) {
+    Due.prototype._init = function (options:options) {
+        // 创建一些私有属性
+        const vm = this;
+        this.uid = uid ++;
+        this._isDue = true;
+
+        // 运行生命周期函数，配置相关属性
+        // 1.beforeCreated 钩子函数
+        console.log('beforeCreated');
+        // 2.注入 props data computed methods provide inject 等等
+        // 最后使用代理模式挂载到实例中
+            // 2.1 代理 data
+        if(options && options.data) {
+            vm._data = constructorProxy(options.data);
+        }
+    }
+}
---
+++ Due/type/options.ts
@@ -0,0 +1,16 @@
+/*
+ * @Description: Due 构造函数参数限制
+ * @Author: PhilRandWu
+ * @Github: https://github/PhilRandWu
+ * @Date: 2022-05-06 16:40:30
+ * @LastEditTime: 2022-05-06 16:53:15
+ * @LastEditors: PhilRandWu
+ */
+export interface options {
+    data?: {}
+}
+
+
+export interface vm {
+    _data: {}
+}
