c77fc9c1940b44cbfc91592115d1a4d8026f0d9e
PhilRandWu <3378814052@qq.com>
2022-05-07 17:07:06 +0800

    虚拟dom 显现

--- Due/type/options.ts
+++ Due/type/options.ts
@@ -3,12 +3,13 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-05-06 16:40:30
- * @LastEditTime: 2022-05-07 14:35:23
+ * @LastEditTime: 2022-05-07 16:46:58
  * @LastEditors: PhilRandWu
  */
 export interface options {
     el: string,
-    data?: {} | []
+    data?: {} | [],
+    methods?: {},
 }
 
 
